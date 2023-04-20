846db4e70295f2462c3d3eb990d88633b7e53553
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-04-21 00:05:04 +0100

    Completed exercise 2

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,6 +2,14 @@ namespace GameSix
 {
     public class Foe
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
